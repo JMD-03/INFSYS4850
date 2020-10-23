d5066f9baab01f27f53f328b5099e8172a5fc69c
Sahil Bhakta <sb4y4@mail.umsl.edu>
2020-10-23 11:23:51 -0500

    dfsdf

--- src/times/models.py
+++ src/times/models.py
@@ -1,7 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import datetime
 
 # Create your models here.
-class time(models.Model):
-    user = models.TextField(blank=True) #This will actually be blank=false
-    date = models.DateTimeField(blank=True) #will actually be blank=false
-    manualEntry = models.BooleanField(default=False)
+class timekeep(models.Model):
+    user = models.ForeignKey(User) #This will actually be blank=false
+    c_in = models.DateTimeField(datetime.now()) #will actually be blank=false
+    c_out = models.DateTimeField(null = true, blank = false)
+    clocked = models.BooleanField(default=true)
+
+    #when the clock in button is pushed
+    def clocked_in(self):
+        self.clocked = True
+        self.save()
+        #when the clock out button is pushed
+        def clocked_out(self):
+            self.c_out = timezone.now()
+            self.clocked = False
+            self.save()
+
+            class Meta:
+                get_latest_by = clocked
+                ordering = ['-in_time']
+                #verbose_name = table name
+                #verbose_name_plural = table name plural
+
