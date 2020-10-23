f5879de5f2941ef1d331d0c7fb134fe0cd55f714
Sahil Bhakta <sb4y4@mail.umsl.edu>
2020-10-23 11:24:05 -0500

    Revert "dfsdf"
    This reverts commit d5066f9baab01f27f53f328b5099e8172a5fc69c.

--- src/timecard/settings.py
+++ src/timecard/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'timecard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoproject',
+        'USER': 'dbadminTEST',
+        'PASSWORD': 'dbadminTEST',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
--- src/times/views.py
+++ src/times/views.py
@@ -1,39 +1,6 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.core.urlresolvers import reverse
-from times.models import timekeep
-import datetime
+
 # Create your views here.
-def timeEntry_view(request):
-    #checking to see if the user is actually clocked in
-    try:
-        current_tk = timekeep.objects.get(user = request.user, clocked = True)
-    except timekeep.DoesNotExist:
-        current_tk = None
-    except timekeep.MultipleObjectsReturned:
-        return HttpResponse('an error has occured as multiple clock ins are being recorded')
-
-    #finding monday/first day of the week
-    today = datetime.date.today()
-    last_monday = today - datetime.timedelta(days = today.weekday())
-    recent_time = timekeep.objects.filter(user = request.user, in_time_gte = last_monday)
-    
-    return render(request, "timeEntry.html", {'tk': current_tk, 'user':request.user, 'rs': recent_sessions})
-
-def clock_in(request):
-    try: 
-        tk = timekeep.objects.get(user = request.user, clocked = True)
-        return HttpResponseRedirect(reverse('times:timekeep'))
-    except timekeep.DoesNotExist:
-        tk = timekeep(user = request.user)
-        tk.clocked_in()
-        return HttpResponseRedirect(reverse('times:timekeep'))
-def clock_out(request):
-    try:
-        tk = timekeep.objects.get(user = request.user, clocked = True)
-        tk.clocked_out()
-        return HttpResponseRedirect(reverse('times:timekeep'))
-    except timekeep.DoesNotExist:
-        return HttpResponseRedirect(reverse('times:timekeep'))
-    except timekeep.MultipleObjectsReturned:
-        return HttpResponse('Error: you have clocked in multiple times')
+def timeEntry_view(request, *args, **kwargs):
+    return render(request, "timeEntry.html", {})
